31d096c3b9402056452e15994ef982aafa77f19d
Shimaa Mohamed <65445652+Shimaa0Mohamed@users.noreply.github.com>
2020-05-20 16:41:26 +0200

    Add files via upload

---
+++ play.js
@@ -0,0 +1,13 @@
+// function openForm() 
+    // document.getElementById("open-button").style.display="block";
+    // function closeForm() {
+    //   document.getElementById("open-button").style.display="none";
+    // }
+  document.getElementById("open-button").addEventListener("click" ,function() {
+    document.querySelector(".open-btn").style.display = 'flex';
+  } );
+  function closeForm() {
+ document.querySelector(".open-btn").style.display  = "none";
+}
+
+  
